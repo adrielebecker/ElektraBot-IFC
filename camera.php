3bd40cf1cf359fb369c2571445b9ce4bfa781c54
Adriele Becker <adrielebecker14@gmail.com>
2024-01-06 14:50:41 -0300

    salvar - gravacoes

--- camera.php
+++ camera.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <?php 
-    $pagina = "Câmera";
-    $video = 'video/substituicao-curta.mp4';
     session_start();
+    include 'sql/config.php';
+    $diretorio = "video/substituicao-longa.mp4";
 ?>
 <html lang="pt-BR">
 <head>
@@ -12,12 +12,42 @@
    
 </head>
 <body>
+    <form action="gravacao/acao.php" method="post">
+        <video width='600' controls autoplay muted>
+            <source src='video/substituicao-longa.mp4' type='video/mp4'>
+        </video>
+        <br>
+        Nome
+        <select name="substituicao" id="substituicao" class="form-select border-success text-center" required>
+            <?php
+                try{
+                    $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
+    
+                    $query = "SELECT * FROM substituicao";
+    
+                    $stmt = $conexao->prepare($query);
+                    $stmt->execute();
+                    $substituicao = $stmt->fetchAll();
+                    
+                    foreach($substituicao as $value){
+                        if($id != 0){
+                            echo "<option value='{$value['id']}' selected>{$value['dataSub']}</option>";
+                        } else{
+                            echo "<option value='{$value['id']}'>{$value['nome']}</option>";
+                        }
+                    }
+                }catch(Exception $e){
+                    print("Erro ...<br>".$e->getMessage());
+                    die();
+                }
+                echo "</select><br>";
 
-    <video width='600' controls autoplay muted>
-        <source src='video/substituicao-longa.mp4' type='video/mp4'>
-    </video>
-    <?php
-        echo "<a href='gravacao/acao.php?diretorio=video/substituicao-longa.mp4&idEletricista=".$_SESSION['idEletricista']."'>Salvar</a>";
-    ?>
+                echo "<input type='hidden' name='diretorio' value='{$diretorio}'>";
+            ?>
+
+        <br>
+        <button name="acao" id="acao" value="salvar">Salvar</button>
+
+    </form>
 </body>
 </html>
--- gravacao/acao.php
+++ gravacao/acao.php
@@ -1,3 +1,50 @@
 <?php
-    
+    include '../sql/config.php';
+    session_start();
+
+    $idEletricista = $_SESSION['idEletricista'];
+    $idGerente = $_SESSION['idGerente'];
+    $diretorio = isset($_POST['diretorio']) ? $_POST['diretorio'] : "";
+    $substituicao = isset($_POST['substituicao']) ? $_POST['substituicao'] : "";
+
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'POST':
+            $acao = $_POST['acao'] ? $_POST['acao'] : "";
+            break;
+        case 'GET':
+            $acao = $_GET['acao'] ? $_GET['acao'] : "";
+            break;
+    }
+
+    switch ($acao) {
+        case 'salvar':
+            salvar();
+            break;
+    }
+
+    function bindar($stmt){
+        global $idGerente, $idEletricista, $diretorio, $substituicao;
+
+        $stmt->bindValue(":gerente", $idGerente);
+        $stmt->bindValue(":eletricista", $idEletricista);
+        $stmt->bindValue(":video", $diretorio);
+        $stmt->bindValue(":substituicao", $substituicao);
+    }
+
+    function salvar(){
+        try{
+            $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
+
+            $query = "INSERT INTO gravacao(eletricista, gerente, video, substituicao) VALUES(:eletricista, :gerente, :video, :substituicao)";
+
+            $stmt = $conexao->prepare($query);
+
+            bindar($stmt);
+
+            $stmt->execute();
+        }catch(Exception $e){
+            print("Erro ...<br>".$e->getMessage());
+            die();
+        }
+    }
 ?>
