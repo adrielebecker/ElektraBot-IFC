ddb18641bc1ed077cd6bb6595f8c22b585de8490
Adriele Becker <adrielebecker14@gmail.com>
2024-01-25 15:24:04 -0300

    pesquisa em relatorios gerente

--- relatorio/relatorios-gerente.php
+++ relatorio/relatorios-gerente.php
@@ -62,42 +62,73 @@
             </div>
         </div>
 
-        <div class="row mt-3">
-            <?php
-                try{
-                    $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
-
-                    $query = "SELECT  texto, codAntigo, codNovo, tipo, acidente, relatorio.gerente, substituicao, substituicao.nome, substituicao.id, relatorio.id FROM substituicao, relatorio WHERE substituicao.id = relatorio.substituicao";
-    
-                    $stmt = $conexao->prepare($query);
-                    $stmt->execute();
-                    $relatorios = $stmt->fetchAll();
-                    
-                    foreach($relatorios as $relatorio){
-                        if($_SESSION['idGerente'] === $relatorio['gerente']){
-                            if($relatorio == NULL){
-                                echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
-                                    <div class='col-4'></div>
-                                    <div class='col-4 ms-4 mt-4 bg-image'>
-                                        <img src='../img/icones/pasta.png' width='60%' class='img-relatorio ms-5'>
-                                    </div>";
-                                break;
-                            } else{
-                                echo "<div class='col-2 mt-4 text-center'>
-                                    <a href='visualizar-gerente.php?relatorio={$relatorio['id']}' class='link texto fs-5 text-reset'><img src='../img/icones/pastaRelatorio.png'></a>
-                                    <p class='texto preto fs-6'>".ucWords($relatorio['nome'])."</p>
-                                </div>";
-                            }
-                        } 
-                    }
-                } catch(Exception $e){
-                    print("Erro ...<br>".$e->getMessage());
-                    die();
-                
-                }
-            ?>
+        <div class="row">
+            <div class="col-12 mt-1">
+                <h5 class="text-center titulo verde">Pesquisar Relatório:</h5>
             </div>
         </div>
-    </div>
+
+        <div class="row mt-3 text-center">
+            <div class="col-3"></div>
+            <div class="col-4">
+                <form action="" method="post">
+                    <input type="text" name="busca" id="busca" placeholder="Pesquise pelo nome" class="form-control text-center border-success">
+            </div>
+            <div class="col-2">
+                    <input type="submit" name="acao" id="acao" class="btn btn-success" value="Fazer a consulta">
+                </form> 
+            </div>
+        </div>
+            
+        <div class="col-12">
+            <div class="row mt-4">
+                <?php
+                    try{
+                        $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
+
+                        $busca = isset($_POST['busca']) ? $_POST['busca']: "";
+                        $query = "SELECT  texto, codAntigo, codNovo, tipo, acidente, relatorio.gerente, substituicao, substituicao.nome, substituicao.id, relatorio.id FROM substituicao, relatorio WHERE substituicao.id = relatorio.substituicao";
+                        
+                        if ($busca != ""){
+                            $busca = $busca.'%';
+                            $query .= ' AND nome like :busca' ;
+                        }
+
+                        $stmt = $conexao->prepare($query);
+
+                        if ($busca != ""){
+                            $stmt->bindValue(':busca',$busca);
+                        }
+
+                        $stmt->execute();
+                        $relatorios = $stmt->fetchAll();
+
+                        foreach($relatorios as $relatorio){
+                            if($_SESSION['idGerente'] === $relatorio['gerente']){
+                                if($relatorio == NULL){
+                                    echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
+                                        <div class='col-4'></div>
+                                        <div class='col-4 ms-4 mt-4 bg-image'>
+                                            <img src='../img/icones/pasta.png' width='60%' class='img-relatorio ms-5'>
+                                        </div>";
+                                    break;
+                                } else{
+                                    echo "<div class='col-2 mt-4 text-center'>
+                                        <a href='visualizar-gerente.php?relatorio={$relatorio['id']}' class='link texto fs-5 text-reset'><img src='../img/icones/pastaRelatorio.png'></a>
+                                        <p class='texto preto fs-6'>".ucWords($relatorio['nome'])."</p>
+                                    </div>";
+                                }
+                            } 
+                        }
+                        
+
+                    }catch(PDOExeptio $e){
+                        print("Erro ao conectar com o banco de dados . . . <br>".$e->getMenssage());
+                        die();
+                    }
+                ?>
+            </div>
+        </div>
+    </div>  
 </body>
 </html>
