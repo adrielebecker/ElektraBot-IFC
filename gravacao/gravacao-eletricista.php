b19edc8239a42ac16e360ffaafbae91da199c2df
Adriele Becker <adrielebecker14@gmail.com>
2024-01-29 20:37:02 -0300

    sem gravacoes

--- gravacao/gravacao-eletricista.php
+++ gravacao/gravacao-eletricista.php
@@ -37,19 +37,23 @@
                                 $stmt->execute();
                                 $gravacoes = $stmt->fetchAll();
                                 
-                                foreach($gravacoes as $gravacao){
-                                    if($_SESSION['idEletricista'] === $gravacao['eletricista']){
-                                        if($gravacao == NULL){
-                                            echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
-                                            break;
-                                        } else{
-                                            echo "<div class='border border-success rounded mt-2 text-center'>
-                                                    <a href='video-eletricista.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'>
-                                                        <p class='texto mt-3'><b class='verde'>".ucWords($gravacao['3'])."</b></p>
-                                                    </a>
-                                            </div>";
-                                        }
-                                    } 
+                                if(empty($gravacoes)){
+                                    echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
+                                } else{
+                                    foreach($gravacoes as $gravacao){
+                                        if($_SESSION['idEletricista'] === $gravacao['eletricista']){
+                                            if($gravacao == NULL){
+                                                echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
+                                                break;
+                                            } else{
+                                                echo "<div class='border border-success rounded mt-2 text-center'>
+                                                        <a href='video-eletricista.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'>
+                                                            <p class='texto mt-3'><b class='verde'>".ucWords($gravacao['3'])."</b></p>
+                                                        </a>
+                                                </div>";
+                                            }
+                                        } 
+                                    }
                                 }
                             } catch(Exception $e){
                                 print("Erro ...<br>".$e->getMessage());
@@ -104,22 +108,30 @@
                         $stmt->execute();
                         $gravacoes = $stmt->fetchAll();
                         
-                        foreach($gravacoes as $gravacao){
-                            if($_SESSION['idEletricista'] === $gravacao['eletricista']){
-                                if($gravacao == NULL){
-                                    echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
-                                        <div class='col-4'></div>
-                                        <div class='col-4 ms-4 mt-4 bg-image'>
-                                            <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
+                        if(empty($gravacoes)){
+                            echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
+                                <div class='col-4'></div>
+                                <div class='col-4 ms-4 mt-4 bg-image'>
+                                    <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
+                                </div>";
+                        } else{
+                            foreach($gravacoes as $gravacao){
+                                if($_SESSION['idEletricista'] === $gravacao['eletricista']){
+                                    if(empty($gravacao)){
+                                        echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
+                                            <div class='col-4'></div>
+                                            <div class='col-4 ms-4 mt-4 bg-image'>
+                                                <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
+                                            </div>";
+                                        break;
+                                    } else{
+                                        echo "<div class='col-2 mt-4 text-center'>
+                                            <a href='video-eletricista.php?video={$gravacao['video']}&nome={$gravacao['nome']}' class='link texto fs-5 text-reset'><img src='../img/icones/video.png'></a>
+                                            <p class='texto fs-6'>".ucWords($gravacao['nome'])."</p>
                                         </div>";
-                                    break;
-                                } else{
-                                    echo "<div class='col-2 mt-4 text-center'>
-                                        <a href='video-eletricista.php?video={$gravacao['video']}&nome={$gravacao['nome']}' class='link texto fs-5 text-reset'><img src='../img/icones/video.png'></a>
-                                        <p class='texto fs-6'>".ucWords($gravacao['nome'])."</p>
-                                    </div>";
-                                }
-                            } 
+                                    }
+                                } 
+                            }
                         }
                         
 
--- gravacao/gravacao-gerente.php
+++ gravacao/gravacao-gerente.php
@@ -37,19 +37,23 @@
                                 $stmt->execute();
                                 $gravacoes = $stmt->fetchAll();
                                 
-                                foreach($gravacoes as $gravacao){
-                                    if($_SESSION['idGerente'] === $gravacao['gerente']){
-                                        if($gravacao == NULL){
-                                            echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
-                                            break;
-                                        } else{
-                                            echo "<div class='border border-success rounded mt-2 text-center'>
-                                                    <a href='video-gerente.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'>
-                                                        <p class='texto mt-2'><b class='verde'>".ucWords($gravacao['3'])."</b> <br> <i class='tam10'> Eletricista: <br>".ucWords($gravacao['nome'])."</i></p>
-                                                    </a>
-                                            </div>";
-                                        }
-                                    } 
+                                if(empty($gravacoes)){
+                                    echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
+                                } else{
+                                    foreach($gravacoes as $gravacao){
+                                        if($_SESSION['idGerente'] === $gravacao['gerente']){
+                                            if($gravacao == NULL){
+                                                echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>";
+                                                break;
+                                            } else{
+                                                echo "<div class='border border-success rounded mt-2 text-center'>
+                                                        <a href='video-gerente.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'>
+                                                            <p class='texto mt-2'><b class='verde'>".ucWords($gravacao['3'])."</b> <br> <i class='tam10'> Eletricista: <br>".ucWords($gravacao['nome'])."</i></p>
+                                                        </a>
+                                                </div>";
+                                            }
+                                        } 
+                                    }
                                 }
                             } catch(Exception $e){
                                 print("Erro ...<br>".$e->getMessage());
@@ -103,26 +107,32 @@
 
                         $stmt->execute();
                         $gravacoes = $stmt->fetchAll();
-                        
-                        foreach($gravacoes as $gravacao){
-                            if($_SESSION['idGerente'] === $gravacao['gerente']){
-                                if($gravacao == NULL){
-                                    echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
-                                        <div class='col-4'></div>
-                                        <div class='col-4 ms-4 mt-4 bg-image'>
-                                            <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
-                                        </div>";
-                                    break;
-                                } else{
-                                    echo "<div class='col-2 mt-4 text-center'>
-                                        <a href='video-gerente.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'><img src='../img/icones/video.png'></a>
-                                        <p class='texto'><b>".ucWords($gravacao['3'])."</b> <br> <i class='tam10'> Eletricista: <br>".ucWords($gravacao['nome'])."</i></p>
-                                    </div>";
-                                }
-                            } 
-                        }
-                        
 
+                        if(empty($gravacoes)){
+                            echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
+                                <div class='col-4'></div>
+                                <div class='col-4 ms-4 mt-4 bg-image'>
+                                    <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
+                                </div>";
+                        } else{
+                            foreach($gravacoes as $gravacao){
+                                if($_SESSION['idGerente'] === $gravacao['gerente']){
+                                    if(empty($gravacao)){
+                                        echo "<h4 class='text-center titulo mt-5'>Ainda não há gravações!</h4>
+                                            <div class='col-4'></div>
+                                            <div class='col-4 ms-4 mt-4 bg-image'>
+                                                <img src='../img/icones/video.png' width='60%' class='img-gravacao ms-5'>
+                                            </div>";
+                                        break;
+                                    } else{
+                                        echo "<div class='col-2 mt-4 text-center'>
+                                            <a href='video-gerente.php?video={$gravacao['video']}&nome={$gravacao['3']}&eletricista={$gravacao['eletricista']}' class='link texto fs-5 text-reset'><img src='../img/icones/video.png'></a>
+                                            <p class='texto'><b>".ucWords($gravacao['3'])."</b> <br> <i class='tam10'> Eletricista: <br>".ucWords($gravacao['nome'])."</i></p>
+                                        </div>";
+                                    }
+                                } 
+                            }
+                        }
                     }catch(PDOExeptio $e){
                         print("Erro ao conectar com o banco de dados . . . <br>".$e->getMenssage());
                         die();
