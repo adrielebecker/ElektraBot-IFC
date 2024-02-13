236695f9043f8064a60d29684eb4778ce31a11c3
Adriele Becker <adrielebecker14@gmail.com>
2024-02-12 22:25:04 -0300

    mapa em eletricista

--- substituicao/visualizar-eletricista.php
+++ substituicao/visualizar-eletricista.php
@@ -13,6 +13,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$pagina?></title>
     <?php include '../eletricista/link.html';?>
+    <style>
+        #map {
+            height: 400px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <?php include '../navbar/nav-eletricista.php'; ?>
@@ -333,31 +339,35 @@
                         <h6 class="texto verde text-center ms-5">LOCALIZAÇÃO</h6>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-2"></div>
-                    <div class="col-10 mt-1">
-                        <?php
-                            try{
-                                $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
-                
-                                $query = "SELECT * FROM substituicao";
-                
-                                $stmt = $conexao->prepare($query);
-                                $stmt->execute();
-                                $substituicoes = $stmt->fetchAll();
-                                
-                                foreach($substituicoes as $substituicao){
-                                    if($idSubstituicao == $substituicao['id']){
-                                        echo "<iframe class='border border-success rounded-3' src='".$substituicao['localizacao']."' width='400' height='300' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                    }
-                                }
-                            }catch(Exception $e){
-                                print("Erro ...<br>".$e->getMessage());
-                                die();
+                <div id="map"></div>
+                <?php
+                    try{
+                        $conexao = new PDO(MYSQL_DSN,USER,PASSWORD);
+        
+                        $query = "SELECT * FROM substituicao";
+        
+                        $stmt = $conexao->prepare($query);
+                        $stmt->execute();
+                        $substituicoes = $stmt->fetchAll();
+                        
+                        foreach($substituicoes as $substituicao){
+                            if($idSubstituicao == $substituicao['id']){
+                                echo "<script>
+                                var map = L.map('map').setView([-27.217712470118357, -49.64618682861328], 12);
+            
+                                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                    attribution: '© OpenStreetMap contributors'
+                                }).addTo(map);
+            
+                                L.marker([".ucWords($substituicao['latitude']).",".ucWords($substituicao['longitude'])."], {draggable: true}).addTo(map);
+                                </script>";
                             }
-                        ?>
-                    </div>
-                </div>
+                        }
+                    }catch(Exception $e){
+                        print("Erro ...<br>".$e->getMessage());
+                        die();
+                    }
+                ?> 
             </div>
         </div>
     </div>
